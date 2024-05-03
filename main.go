3a5c8b750c2d23f1ada7ca5bb7c038dda5f75e98
6ApostaL9 <168728822+6ApostaL9@users.noreply.github.com>
2024-05-03 17:32:21 +0500

    Update main.go

--- main.go
+++ main.go
@@ -7,15 +7,32 @@ import (
 	"strings"
 )
 
+// Проверка на пустоту ввода
+func checkEmptyInput(inputString string) {
+	if len(inputString) == 0 {
+		fmt.Println("Выдача паники, так как выражение не введено") //Обработка пустой строки
+		os.Exit(1)
+	}
+}
+
+// Проверка на корректность выражения
+func checkCorrectExpression(arrInputString []string) {
+	//Проверка длины выражения
+	if len(arrInputString) != 3 {
+		fmt.Println("Выдача паники, так как формат математической операции не удовлетворяет заданию — два операнда и один оператор (+, -, /, *).")
+		os.Exit(2)
+	}
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	inputString, _ := reader.ReadString('\n')    //Ждет ввода данных в формате строки
 	inputString = strings.TrimSpace(inputString) //Очищает все пробелы
 
-	if len(inputString) == 0 {
-		fmt.Println("Выдача паники, так как строка пустая") //Обработка пустой строки
-		os.Exit(1)
-	}
+	checkEmptyInput(inputString)
+
+	arrInputString := strings.Split(inputString, " ") //Преобразование строки в отдельные символы
+	checkCorrectExpression(arrInputString)
 
 	os.Exit(0)
 }
